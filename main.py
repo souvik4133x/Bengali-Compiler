18d846629156c785a43e8f524e29e107cd1f719b
Souvik Saha <souviksaha7789@gmail.com>
2025-09-21 07:41:39 +0530

    updated

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Bengali Script API")
 # Enable CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bengali-compiler-mgyr6ui6g-souvik-sahas-projects.vercel.app"],
+    allow_origins=["https://bengali-compiler.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
